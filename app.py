4357128312a540b7a36e6da79b41c1a2fd8fdf83
lordofsunshine <94736527+lordofsunshine@users.noreply.github.com>
2025-02-07 06:11:50 -0800

    Start

--- app.py
+++ app.py
@@ -194,5 +194,7 @@ async def after_request(response):
     response.headers['Access-Control-Expose-Headers'] = 'Content-Length'
     return response
 
+wsgi_app = app
+
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000) 
